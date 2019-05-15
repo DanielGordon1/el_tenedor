49d674c90ff27e42e09d142985992c985e204cc0
Daniel Gordon <d_gordon1@icloud.com>
2019-05-15 09:54:17 +0200

    created collection and member routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    collection do
+      get 'top'
+      # get 'worst'
+      # get 'latest'
+    end
+    member do
+      get 'chef'
+    end
+  end
+
+  # get 'restaurants/top', to: 'restaurants#top', as: 'top_restaurants'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
